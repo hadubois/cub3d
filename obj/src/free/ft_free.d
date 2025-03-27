2753421de5ac4517f4df1e4ca11bc06661dbb6df
Rellotropcool <rellotropcool@gmail.com>
2025-03-27 18:22:53 +0100

    1st tests of raycasting

--- obj/src/free/ft_free.d
+++
@@ -1,7 +0,0 @@
-obj/src/free/ft_free.o: src/free/ft_free.c includes/cube3d.h \
- includes/../lib/get_next_line/get_next_line.h \
- includes/../lib/libft/libft.h includes/../mlx/mlx.h
-includes/cube3d.h:
-includes/../lib/get_next_line/get_next_line.h:
-includes/../lib/libft/libft.h:
-includes/../mlx/mlx.h:
